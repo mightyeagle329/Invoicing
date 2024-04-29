719779ed227813ff9438c66f76736e5d0ad15b4d
mightyeagle329 <cawsonoliver33@gmail.com>
2024-04-29 19:17:49 +0100

    fix: resolve issue with omitted props and data

--- components/invoice/InvoiceDetails.tsx
+++ components/invoice/InvoiceDetails.tsx
@@ -17,44 +17,45 @@ export default function InvoiceDetails({
     <div className="flex flex-col gap-6 flex-1 sm:min-h-screen max-w-[1200px] w-full mx-auto">
       <div className="flex flex-col sm:flex-row justify-between items-end gap-4 pt-9 sm:pt-16 lg:pt-20">
         <div className="flex flex-col gap-2">
-          <h1>{invoice.businessName}</h1>
+          {/*  <h1>{invoice.businessName}</h1>
           <p>{invoice.businessEmail}</p>
           <p>{`Invoice #${invoice._id}`}</p>
-          <p>{format(invoice.invoiceDate, "PPP")}</p>
+          <p>{format(invoice.invoiceDate, "PPP")}</p> */}
           <Status status={invoice.status} />
         </div>
         <div className="flex flex-wrap sm:items-center gap-4">
           {invoice.status !== "paid" && <MarkAsPaid />}
-          <EditInvoice invoiceId={invoice._id} />
-          <DeleteInvoice invoiceId={invoice._id} onDelete={onDelete} />
+          {/*  <EditInvoice invoiceId={invoice._id} />
+          <DeleteInvoice invoiceId={invoice._id} onDelete={onDelete} /> */}
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 items-end gap-4">
         <div className="flex flex-col gap-2">
           <h2>Bill From</h2>
           <Separator />
-          <p>{invoice.businessName}</p>
+          {/* <p>{invoice.businessName}</p>
           <p>{invoice.businessEmail}</p>
           <p>{invoice.businessAddress}</p>
           <p>{invoice.businessCity}</p>
           <p>{invoice.businessZip}</p>
-          <p>{invoice.businessCountry}</p>
+          <p>{invoice.businessCountry}</p> */}
         </div>
         <div className="flex flex-col gap-2">
           <h2>Bill To</h2>
           <Separator />
-          <p>{invoice.clientName}</p>
+          {/* <p>{invoice.clientName}</p>
           <p>{invoice.clientEmail}</p>
           <p>{invoice.clientAddress}</p>
           <p>{invoice.clientCity}</p>
           <p>{invoice.clientZip}</p>
-          <p>{invoice.clientCountry}</p>
+          <p>{invoice.clientCountry}</p> */}
         </div>
       </div>
       <div className="flex flex-col gap-2">
         <h2>Project</h2>
         <Separator />
-        <p>{invoice.projectName}</p>
+        {/*         <p>{invoice.projectName}</p>
+         */}{" "}
         <h3>Deliverables</h3>
       </div>
     </div>
--- components/invoice/InvoiceItem.tsx
+++ components/invoice/InvoiceItem.tsx
@@ -4,25 +4,25 @@ import { InvoiceData } from "@/lib/types/data";
 import { format } from "date-fns";
 
 export default function InvoiceItem({
-  invoiceDate,
-  paymentDue,
-  clientName,
-  businessName,
-  total,
-  status,
+  /*  invoiceDate,
+   paymentDue,
+   clientName,
+   businessName,
+   total,
+   status, */
   shouldDisplayInvoice,
 }: InvoiceData & { shouldDisplayInvoice?: (status: string) => boolean }) {
   if (!shouldDisplayInvoice || shouldDisplayInvoice(status))
     return (
       <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,1fr))] items-center justify-between">
-        <p className="whitespace-nowrap overflow-hidden text-ellipsis">
+        {/* <p className="whitespace-nowrap overflow-hidden text-ellipsis">
           {format(invoiceDate, "PPP")}
         </p>
         <p>{paymentDue}</p>
         <p>{businessName}</p>
         <p>{clientName}</p>
         <p className="font-bold text-[0.9375rem]">{formatCurrency(total)}</p>
-        <Status status={status} />
+        <Status status={status} /> */}
       </div>
     );
 }
