0fcbfa40395e187f189f0402e9e1a72f1f1857f2
mightyeagle329 <cawsonoliver33@gmail.com>
2024-05-06 17:32:20 +0100

    fix: resolve missing fetcher function

--- lib/functions/fetcher.ts
+++ lib/functions/fetcher.ts
@@ -1,17 +1,4 @@
-/* export const fetcher = async (...args: [RequestInfo, RequestInit?]) => {
+export const fetcher = async (...args: [RequestInfo, RequestInit?]) => {
   const res = await fetch(...args);
   return res.json();
 };
-
-export async function getInvoices() {
-  const res = await fetch(`${process.env.API_BASE_URL}/lib/data/orders.ts`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch order data");
-  }
-
-  return res.json();
-}
- */
